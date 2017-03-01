6f1757f794f981658db352c54a5ace88274b68a3
craig <cbenerofe@yahoo.com>
2017-02-28 22:43:50 -0500

    in progress

--- app.js
+++ app.js
@@ -1,12 +1,78 @@
 var express = require('express')
+var stormpath = require('express-stormpath');
+
 var app = express()
 
 app.set('views', './views')
 app.set('view engine', 'pug')
 
-app.get('/', function (req, res) {
+var server= "http://localhost:3000"
+var user_enpoint = server + "api/user"
+
+app.use(express.static('public'));
+app.use(stormpath.init(app, {
+  client: {
+    apiKey: {
+      id: '163SGF4XDEZMLJEFTTIRCZ1OJ',
+      secret: 'P9osZnu8sOEQb82iJJyu+aISpONzcT0OdJzd0AkfAKE',
+    }
+  },
+  application: {
+    href: 'https://api.stormpath.com/v1/applications/7I3YUrCjk5LPoesTXaCnBp'
+  },
+
+  postRegistrationHandler: function (account, req, res, next) {
+    console.log('User:', account.email, 'just registered!');
+    // console.log('account:', JSON.stringify(account) );
+    // check if have count.it id & token saved
+    if account.customData.countit_uid == undefined {
+      // check if already registered on count.it
+      // register in count.it or check
+      var resp2 = $.post( url + "/login", {email: email, password: password},function( data ) {
+        uid = data.ident
+        token = data.ident
+      
+        var resp3 = $.get( url, {druser: uid, drauth: token}, function( data ) {
+            user_data = data
+            update()
+            alert( "user data Loaded" );
+          });
+        resp2.fail(function ( data) {
+          alert( "second get fail:" + JSON.stringify(data));
+        });
+      
+      
+    } else {
+      
+      
+    }
+    
+    
+    var resp1 = $.get( url, {druser: uid, drauth: token}, function( data ) {
+      user_data = data
+      update()
+      alert( "user data Loaded" );
+    });
+    
+    
+    next();
+  },
+
+  postLoginHandler: function (account, req, res, next) {
+    console.log('User:', account.email, 'just logged in!');
+    console.log('account:', JSON.stringify(account) );
+    next();
+  }
+}));
+
+
+app.get('/', stormpath.authenticationRequired, function (req, res) {
   res.render('index')
 })
+
+app.on('stormpath.ready', function () {
+  console.log('Stormpath Ready');
+});
 
 app.listen(4000, function () {
   console.log('Example app listening on port 4000!')
